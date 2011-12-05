afa89560bd2fe99db84904269c692f50118934e7
Roberto <rofregui@yahoo.com.br>
2011-12-05 12:05:21 -0200

    Mais uma mensagem

--- Exemplo.java
+++ Exemplo.java
@@ -4,6 +4,7 @@ public class Exemplo {
 
 public static void main(String[] args) {
     System.out.println("Exemplo de teste de uso do git");
+    System.out.println("Trabalhando com branches");
 }
 
 }
